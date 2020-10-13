d9e0e4a8dc5da37f82b0d03b78ccc70bfa9c6b98
Glenn <hazelnutvt04@gmail.com>
2020-10-12 21:26:23 -0400

    Added Python script for FFT reference

---
+++ try_fft.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import pathlib
+
+# Script for try_fft
+
+def get_samples( filepath, num_samples, debug=False ):
+    samples = np.fromfile( filepath, dtype = np.complex64, count = num_samples, sep=""  )
+    #  if debug: print( "num_samples is {}".format( num_samples ) )
+    #  if debug: print( "filepath is is {}".format( filepath ) )
+    #  if debug: print( "Size of samples is {}".format( len(samples) ) )
+    return samples
+
+def try_fft( filename, num_samples, fft_size, debug=False ):
+    filepath = str( pathlib.Path().absolute()) + "/" + filename
+    samples = get_samples( filename, num_samples, debug )
+    start_index = 0
+    while start_index < num_samples:
+        frequencies = np.fft.fft( samples[start_index: start_index + fft_size], fft_size )
+        sfrequencies = np.fft.fftshift( frequencies )
+        #  if debug: print( "start_index: {}: samples is {}".format( start_index,
+        #      samples[ start_index: start_index + fft_size] ) )
+        if debug: print( "start_index: {}: frequencies is {}".format( start_index, frequencies ) )
+        start_index += fft_size
+
+if __name__ == '__main__':
+    filename = 'testdataBPSKcomplex.bin'
+    num_samples = 32768
+    fft_size = 64
+    debug = True
+    try_fft( filename, num_samples, fft_size, debug ) 
