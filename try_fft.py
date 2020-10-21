Fixed bug in psd calculation. Also fixed bug in try_fft.py where it did not generate enough expected conjugate squares and psds

try_fft.py
@@ -7,10 +7,29 @@ import pathlib
 
 def get_samples( filepath, num_samples, debug=False ):
     samples = np.fromfile( filepath, dtype = np.complex64, count = num_samples, sep=""  )
-    #  if debug: print( "num_samples is {}".format( num_samples ) )
-    #  if debug: print( "filepath is is {}".format( filepath ) )
-    #  if debug: print( "Size of samples is {}".format( len(samples) ) )
+    if debug: print( "num_samples is {}".format( num_samples ) )
+    if debug: print( "filepath is is {}".format( filepath ) )
+    if debug: print( "Size of samples is {}".format( len(samples) ) )
     return samples
+
+def write_expected_file( filename, exp_type, exp_name, exp_data, debug=False ):
+    with open(filename,'w') as exp_file:
+        exp_data_len = len(exp_data)
+        exp_file.write( "#pragma once\n" )
+        if exp_type == 'cufftComplex': exp_file.write( "\n#include <cufft.h>\n" )
+        exp_file.write( "\nconstexpr {} {}[] = {{".format( exp_type, exp_name ) )
+        index = 0
+        for exp in exp_data:
+            if exp_type == 'cufftComplex':
+                comp_exp = complex(exp)
+                exp_file.write( "\t{{{},{}}}{}\n".format( exp.real, exp.imag, (",","")[index >= exp_data_len-1] ) )
+            else:
+                if exp == float('-Inf'): exp = 0
+                exp_file.write( "\t{}{}\n".format( exp, (",","")[index >= exp_data_len-1] ) )
+            index += 1
+        exp_file.write( "};\n\n" )
+
+
 
 def try_fft( filename, num_samples, fft_size, debug=False ):
     filepath = str( pathlib.Path().absolute()) + "/" + filename
@@ -26,7 +45,8 @@ def try_fft( filename, num_samples, fft_size, debug=False ):
         frequencies = np.fft.fft( samples[start_index: start_index + fft_size], fft_size )
         frequencies = np.fft.fftshift( frequencies )
         con_sqrs = np.square(np.real(frequencies)) + np.square(np.imag(frequencies))
-        psds = 10*np.log10(con_sqrs) - 10*np.log10(num_samples)
+        fft_sizes = np.full( (1, fft_size), fft_size)
+        psds = 10*np.log10(con_sqrs) - 10*np.log10(fft_sizes)
         if debug: 
             print( "start_index: {}: samples is {}".format( start_index,
                 samples[ start_index: start_index + fft_size] ) )
@@ -44,36 +64,19 @@ def try_fft( filename, num_samples, fft_size, debug=False ):
     all_con_sqrs = all_con_sqrs.flatten()
     all_psds = np.array( all_psds )
     all_psds = all_psds.flatten()
-    #all_frequencies = all_frequencies.view( np.float32 )
 
-    print( "#pragma once\n" )
-    print( "\n#include <cufft.h>\n" )
-    
-    # print( "constexpr cufftComplex expected_frequencies[] = {" )
-    # index = 0
-    # for frequency in all_frequencies:
-    #     print( "\t{}{} ".format( frequency, (",","")[index >= num_samples-1] ) )
-    #     index += 1
-    # print( "};\n\n" )
-    
-    # print( "constexpr float expected_con_sqrs[] = {" )
-    # index = 0
-    # for con_sqr in all_con_sqrs:
-    #     print( "\t{}{} ".format( con_sqr, (",","")[index >= num_samples-1] ) )
-    #     index += 1
-    # print( "};\n" )
-    
-    # print( "constexpr float expected_psds[] = {" )
-    # index = 0
-    # for psd in all_psds:
-        # print( "\t{}{} ".format( psd, (",","")[index >= num_samples-1] ) )
-        # index += 1
-    # print( "};\n" )
-    
+    freq_filename = '/home/glenn/Programming/CUDA/dev_func_fft/include/expected_sfrequencies.hpp.in'
+    con_sqrs_filename = '/home/glenn/Programming/CUDA/dev_func_fft/include/expected_con_sqrs.hpp.in'
+    psds_filename = '/home/glenn/Programming/CUDA/dev_func_fft/include/expected_psds.hpp.in'
+
+    write_expected_file( freq_filename, 'cufftComplex', 'expected_sfrequencies', all_frequencies, debug )
+    write_expected_file( con_sqrs_filename, 'float', 'expected_con_sqrs', all_con_sqrs, debug )
+    write_expected_file( psds_filename, 'float', 'expected_psds', all_psds, debug )    
+
 
 if __name__ == '__main__':
     filename = 'testdataBPSKcomplex.bin'
     num_samples = 32768
     fft_size = 64
-    debug = False
+    debug = True
     try_fft( filename, num_samples, fft_size, debug ) 